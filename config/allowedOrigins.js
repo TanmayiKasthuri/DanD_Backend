b663ac69ba50f6fb6454a86739f9557133439d45
Tanmayi Kasthuri <tanmayi.kasthuri@gmail.com>
2023-12-13 21:51:23 +0530

    changes made in this file to deploy

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,7 @@
 const allowedOrigins=[
-    'http://localhost:3000',
-    'https://www.google.com'
+    // 'http://localhost:3000',
+    // 'https://www.google.com'
+    'https://dandrepair.onrender.com/'//for deployment
 ]
 
 module.exports=allowedOrigins
